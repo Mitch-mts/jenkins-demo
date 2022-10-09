c213c6c6c6e156f289a4d6e712abd735580c1095
mitchell.severa <mitchell.severa@entelect.co.za>
2022-10-09 15:46:08 +0200

    initial commit

---
+++ src/main/java/com/example/sunday/SundayApplication.java
@@ -0,0 +1,18 @@
+package com.example.sunday;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SundayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SundayApplication.class, args);
+        test();
+    }
+    
+    public static void test(){
+        System.out.println("Hello, have a blessed Sunday ");
+    }
+
+}
